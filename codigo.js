937ceee3d26d12da95a277abd8a1617596bf7cfd
Facundo Aranda <facundor.aranda@gmail.com>
2024-03-19 02:12:01 -0300

    modificaciones js y css

--- codigo.js
+++ codigo.js
@@ -1,19 +1,28 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+
+
 function saludar() {
     let nombre1 = prompt("Ingresa tu nombre");
     alert("Buenas noches, " + nombre1);
 }
 saludar();
 
-function calculadoraIMC(peso, altura) {
+let nombre = prompt("Ingresa nombre del paciente");
+let apellido = prompt("Ingresa apellido del paciente");
+let altura = parseFloat(prompt("Ingresa altura del paciente en metros"));
+let peso = parseInt(prompt("Ingresa peso del paciente en kg"));
+let edad = parseInt(prompt("Ingresa tu edad"));
+
+function calculadoraIMC (peso, altura) {
     let indice = peso / (altura * altura);
     if (indice < 19) {
-        alert("El indice IMC es de: " + indice.toFixed(2)+". Necesitas ganar peso. En este momento, es posible que estés experimentando los siguientes síntomas: fatiga, estrés, ansiedad. Busca a una nutricionista.");
+        alert(`El indice IMC es de: ${indice.toFixed(2)}.Necesitas ganar peso. En este momento, es posible que estés experimentando los siguientes síntomas: fatiga, estrés, ansiedad. Busca a una nutricionista.`);
     } else if (19 < indice && indice < 24) {
-        alert("El indice IMC es de: " + indice.toFixed(2)+". ¡Estás dentro del peso adecuado! Tienes un menor riesgo de enfermedades cardíacas y vasculares.");
+        alert(`El indice IMC es de: ${indice.toFixed(2)}.¡Estás dentro del peso adecuado! Tienes un menor riesgo de enfermedades cardíacas y vasculares.`);
     } else if (24 < indice && indice > 30) {
-        alert("El indice IMC es de: " + indice.toFixed(2)+". ¡Estás con sobrepeso! En este momento, es posible que estés experimentando los siguientes síntomas: fatiga, mala circulación, várices. ¡Busca a una nutricionista!")
+        alert(`El indice IMC es de: ${indice.toFixed(2)}.¡Estás con sobrepeso! En este momento, es posible que estés experimentando los siguientes síntomas: fatiga, mala circulación, várices. ¡Busca a una nutricionista!`);
     } else if (30 < indice) {
-        alert("El indice IMC es de: " + indice.toFixed(2)+". ¡Estás en el rango de Obesidad Morbidad! En este momento, es posible que estés experimentando los siguientes síntomas: reflujo, dificultad para moverte, úlceras por presión, diabetes, ataque al corazón, accidente cerebrovascular. ¡Busca a una nutricionista!")
+        alert(`El indice IMC es de: ${indice.toFixed(2)}.¡Estás en el rango de Obesidad Morbidad! En este momento, es posible que estés experimentando los siguientes síntomas: reflujo, dificultad para moverte, úlceras por presión, diabetes, ataque al corazón, accidente cerebrovascular. ¡Busca a una nutricionista!`);
     }
 };
 
@@ -22,21 +31,14 @@ function mostrarPaciente(nombre, apellido) {
     alert("Nombre: " + nombre + " " + "Apellido: " + apellido);
 }
 
-
-let nombre = prompt("Ingresa nombre del paciente");
-let apellido = prompt("Ingresa apellido del paciente");
-let altura = parseFloat(prompt("Ingresa altura del paciente en metros"));
-let peso = parseInt(prompt("Ingresa peso del paciente en kg"));
-let edad = parseInt(prompt("Ingresa tu edad"));
-
 let opcion = prompt("Ingresa la funcionalidad deseada: \n1-Mostrar datos del paciente\n2-Mostrar IMC del paciente \n0-Salir del Programa");
 while (opcion !== "0") {
     if (opcion == "1") {
-        mostrarPaciente(nombre, apellido, edad);
+        mostrarPaciente (nombre, apellido);
     } else if (opcion == "2") {
-        calculadoraIMC(peso, altura);
+        calculadoraIMC (peso, altura);
     } else {
-        alert("Elije entre las opciones mostradas")
+        alert("Elije entre las opciones")
     }
     opcion = prompt("Ingresa la funcionalidad deseada: \n1-Mostrar datos del paciente\n2-Mostrar IMC del paciente \n0-Salir del Programa");
 }
