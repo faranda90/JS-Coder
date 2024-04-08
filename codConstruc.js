23b0a77eacb1ace409eeb9ad3e9c180506f40142
Facundo Aranda <facundor.aranda@gmail.com>
2024-04-08 01:22:09 -0300

    segunda preentrega

---
+++ codConstruc.js
@@ -0,0 +1,18 @@
+function saludar() {
+    let nombre1 = prompt("Ingresa tu nombre");
+    alert("Buenas noches, " + nombre1);
+}
+saludar();
+
+let mm = parseFloat(prompt("Ingresa los M2 a construir"));
+
+
+function calculaLadr(m2, ladrillo) {
+    let cantidad = mm *15;
+    console.log("Se necesitan " +cantidad + "ladrillos" );
+}
+calculaLadr();
+function revoqInt() {
+    let 
+    
+}
