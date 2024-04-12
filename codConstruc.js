088747878956330984dcfcc1bdd0d0b9454d5322
Facundo Aranda <facundor.aranda@gmail.com>
2024-04-12 01:07:37 -0300

    modificaciones CSS

--- codConstruc.js
+++ codConstruc.js
@@ -1,3 +1,8 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "4vw";
+document.getElementsByTagName("h2")[0].style.fontSize = "2vw";
+document.getElementsByTagName("h3")[0].style.fontSize = "2vw";
+
+
 function saludar() {
     let nombre1 = prompt("Ingresa tu nombre");
     alert("Buenas noches, " + nombre1);
@@ -15,13 +20,17 @@ while (opcion != 0) {
         case 2:
             let tipoConst = parseInt(prompt("Elige el Tipo de Construcción:\n1-Construcción Tradicional\n2-Construcción en Seco"));
             filtrarPorTipo(tipoConst);
+            
             break;
         case 3:
-            let porProd = parseInt(prompt("Elige el producto:\n1-Ladrillo\n2-Cemento\n3-Cal\n4-Arena\n5-Piedra\n6-Montante\n7-Solera\n8-Masilla\n9-Placa de Yeso\n10-Cinta"));
-            mostrarPorProd(porProd);
-        default:
-            alert("Opción Invalida");
+            let nombre =prompt("Escribe el nombre del producto:\n1-Ladrillo\n2-Cemento\n3-Cal\n4-Arena\n5-Piedra\n6-Montante\n7-Solera\n8-Masilla\n9-Placa de Yeso\n10-Cinta");
+            if (nombre != "") {
+                buscarProd(nombre);
+            }else{
+            alert("Ingrese un nombre de producto válido");
+            }
             break;
+
     }
     opcion = parseInt(prompt("Elige la opcion a ejecutar:\n1- Mostrar lista de Productos\n2-Mostrar Productos por tipo de Construcción\n3- Buscar por Productos\n0- Salir"));
 
@@ -39,30 +48,11 @@ function filtrarPorTipo(tipoConstruccion) {
     console.table(tipoConst);
     }
 
-    function mostrarPorProd(nombre) {
-        let porProd = []; 
-        if (nombre == 1) {
-            porProd = productos.filter(producto => producto.nombre.toLowerCase()== "ladrillo");
-        }else if (nombre == 2) {
-            porProd = productos.filter(producto => producto.nombre.toLowerCase()== "cemento");
-        }else if (nombre == 3) {
-            porProd = productos.filter(producto => producto.nombre.toLowerCase()== "cal");
-        }else if (nombre == 4) {
-            porProd = productos.filter(producto => producto.nombre.toLowerCase()== "arena");
-        }else if (nombre == 5) {
-            porProd = productos.filter(producto => producto.nombre.toLowerCase()== "piedra");
-        }else if (nombre == 6) {
-            porProd = productos.filter(producto => producto.nombre.toLowerCase()== "montante");
-        }else if (nombre == 7) {
-            porProd = productos.filter(producto => producto.nombre.toLowerCase()== "solera");
-        }else if (nombre == 8) {
-            porProd = productos.filter(producto => producto.nombre.toLowerCase()== "masilla");
-        }else if (nombre == 9) {
-            porProd = productos.filter(producto => producto.nombre.toLowerCase()== "placa de yeso");
-        }else if (nombre == 10) {
-            porProd = productos.filter(producto => producto.nombre.toLowerCase()== "cinta"); 
-        }else{
-            alert("Ingrese un valor válido");
-        }
-        console.table(porProd);           
+function buscarProd(nombProd) {
+    let productoEncontrado = productos.find(producto => producto.nombre.toLowerCase().includes(nombProd.toLowerCase()));
+    if (productoEncontrado !=undefined) {
+        alert(`La tarea fue encontrada:\nNOMBRE:${productoEncontrado.nombre} \nMedidas: ${productoEncontrado.medida} \nMarca: ${productoEncontrado.marca} \nPrecio: ${productoEncontrado.precio}`);
+    }else {
+        alert("Producto no Encontrado")
     }
+}
